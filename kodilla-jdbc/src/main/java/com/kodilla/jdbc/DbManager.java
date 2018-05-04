2566b29904d2c78194687f8d65e80769636a9afb
Rafal Pijet <stronglopez@wp.pl>
2018-05-04 18:28:34 +0200

    Moduł 22.3 przykład

--- kodilla-jdbc/src/main/java/com/kodilla/jdbc/DbManager.java
+++ kodilla-jdbc/src/main/java/com/kodilla/jdbc/DbManager.java
@@ -11,9 +11,9 @@ public class DbManager {
 
     private DbManager() throws SQLException {
         Properties connectionProps = new Properties();
-        connectionProps.put("user", "kodilla_user");
-        connectionProps.put("password", "kodilla_password");
-        conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/kodilla_cource?serverTimezone=Europe/Warsaw&useSSL=False", connectionProps);
+        connectionProps.put("user", "root");
+        connectionProps.put("password", "");
+        conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/kodilla_course?serverTimezone=Europe/Warsaw&useSSL=False", connectionProps);
     }
 
     public static DbManager getInstance() throws SQLException {
